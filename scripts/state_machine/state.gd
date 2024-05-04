872b57a1dec30798298b48ac03decc80549bb442
AriathGonzalez <84883360+AriathGonzalez@users.noreply.github.com>
2024-05-03 23:16:43 -0600

    Player FSM

---
+++ scripts/state_machine/state.gd
@@ -0,0 +1,34 @@
+class_name State
+extends Node
+
+@export
+var animation_name: String
+@export
+var move_speed: float = 130
+
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var animations: AnimatedSprite2D
+var move_component
+var parent: CharacterBody2D
+
+func enter() -> void:
+	animations.play(animation_name)
+
+func exit() -> void:
+	pass
+
+func process_input(_event: InputEvent) -> State:
+	return null
+
+func process_frame(_delta: float) -> State:
+	return null
+
+func process_physics(_delta: float) -> State:
+	return null
+
+func get_movement_input() -> float:
+	return move_component.get_movement_direction()
+
+func get_jump() -> bool:
+	return move_component.wants_jump()
